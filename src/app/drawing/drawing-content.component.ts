58806f50ca84b66b5dbef6682fb0cb9d35df390b
Rabitto Wong <33476301+rabittowong@users.noreply.github.com>
2021-10-05 00:29:18 +0800

    update drawing

--- src/app/drawing/drawing-content.component.ts
+++ src/app/drawing/drawing-content.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./drawing-content.component.scss'],
 })
 export class DrawingContentComponent {
+  filenames: string[] = [
+    '304.jpg',
+    '303.jpg',
+    '312.jpg',
+    '314.jpg',
+    '406.jpg',
+  ];
+  selectedIndex: number = 0;
+
   constructor() {
+  }
+
+  onChangeSelectedIndex(index: number): void {
+    this.selectedIndex = index;
   }
 }
